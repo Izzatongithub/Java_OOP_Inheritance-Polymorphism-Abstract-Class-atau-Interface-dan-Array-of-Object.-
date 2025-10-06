8d536d9340f0dfcc4cbff5a8962de08f44fd1ea2
F1D02310048 <fadilarahmania04@gmail.com>
2025-10-06 23:08:52 +0800

    update Herbivora.java

--- src/Herbivora.java
+++ src/Herbivora.java
@@ -1,7 +1,7 @@
 public interface Herbivora {
 
     public String jenisH = "Herbivora";
-    public String MakananH = "Tumbuhan hijau";
+    public String MakananH = "Tumbuhan hijau tua";
 
     public void displayMakan();
 }
