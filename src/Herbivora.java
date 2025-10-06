bdf3410789380179e82a9faca9a489ef6ece1289
F1D02310048 <fadilarahmania04@gmail.com>
2025-10-06 22:44:33 +0800

    edit herbivora.java

--- src/Herbivora.java
+++ src/Herbivora.java
@@ -1,6 +1,6 @@
 public interface Herbivora {
 
-    public String jenisH = "Herbivora";
+    public String jenisH = "Herbivoraaa";
     public String MakananH = "Tumbuhan";
 
     public void displayMakan();
